8f983cdb182f6e045d1ab15984f7d49da7565367
Mr Aananda <rmananda007@gmail.com>
2026-06-24 11:21:54 +0600

    fixed image error

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,9 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// Import assets so Vite bundles them and adds them to manifest.json
+import.meta.glob([
+    '../assets/images/**',
+    '../assets/icons/**',
+]);
